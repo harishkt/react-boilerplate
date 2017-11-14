cac2913e28d8f7fe238960d0171fe62da83d9c39
hkumar <hkumar@consultant.underarmour.com>
2017-11-14 12:52:41 -0500

    Updated react boiler plate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './logo.svg';
-import Header from './components/common/header/Header';
-import Footer from './components/common/footer/Footer';
 
 // <img src={logo} className="App-logo" alt="logo" />
 export default class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<Header />
-				{this.props.children}
-				<Footer />
+			<div className="container">
 			</div>
 		);
 	}
